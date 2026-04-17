d24c27a5ab72c8db988079d88ed89caffb75f324
muhammadhasnain3031 <muhammadhasnain3031@gmail.com>
2026-04-17 12:34:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const container = document.getElementById("root");
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(container);
 root.render(<App />);
