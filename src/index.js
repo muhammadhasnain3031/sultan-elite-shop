43d12af890ff2f2f2808cebb531cd4b0579696ee
muhammadhasnain3031 <muhammadhasnain3031@gmail.com>
2026-04-17 12:58:35 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App"; // Kyunke App.jsx isi folder mein hai
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+const root = createRoot(document.getElementById("root"));
 root.render(<App />);
